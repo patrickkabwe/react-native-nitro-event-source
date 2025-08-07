82684edd913a91842ee261f8eb28b2693f2c6dfa
Patrick Kabwe <patrickckabwe@gmail.com>
2025-08-07 11:33:39 +0200

    Update lint-cpp.sh

--- lint-cpp.sh
+++ lint-cpp.sh
@@ -3,5 +3,5 @@
 FILES=$(find cpp -type f \( -name "*.cpp" -o -name "*.c" \))
 for file in $FILES; do
   echo "🔍 Linting $file"
-  clang-tidy "$file" -- -std=c++17
+  clang-tidy "$file" -- -std=c++20
 done
